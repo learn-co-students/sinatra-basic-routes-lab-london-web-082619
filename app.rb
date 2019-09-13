ed09c3cb45fbd4882f127e8e14a3011bbbd11f4d
Mike Thurgood <mikecthurgood@gmail.com>
2019-09-13 21:37:24 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Valthor The Indomitable!"
+    end
+
+    get '/hometown' do
+        "My hometown is New Asguard!"
+    end
+
+    get '/favorite-song' do
+        'My favorite song is Immigrant Song by Led Zeppelin!'
+    end
+
+      
 end
