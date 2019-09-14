fab3664f627fe3b0a17b168c401cc6674aa0188c
Angus Morrison <contact@angus-morrison.com>
2019-09-14 12:56:48 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Angus"
+    end
+
+    get '/hometown' do
+        "My hometown is Bournemouth"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Black Sheep"
+    end
+
 end
+  
