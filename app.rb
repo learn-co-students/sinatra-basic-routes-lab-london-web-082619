9343b58f380d3a4bbe638e7d162b087ca1c0b009
Abdullah Bajaman <ambajaman@gmail.com>
2019-09-15 23:23:41 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get('/name') {
+        "My name is Abdullah"
+    }
+    get('/hometown'){
+        "My hometown is Madinah"
+    }
+    get('/favorite-song'){
+        "My favorite song is All of me"
+    }
 end
