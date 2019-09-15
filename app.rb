db5dc6182398a6cf7b3eedffbac26324b9d834e3
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-15 12:05:44 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello World!"
+    end
+
+    get '/name' do
+        "My name is Elizabeth"
+    end
+
+    get '/hometown' do
+        "My hometown is Victoria, Canada"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Time by Hans Zimmer"
+    end
+
+
 end
